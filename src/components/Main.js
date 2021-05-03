3fa86798c7f5be32f42971f89b92c658eaa23afc
200DRM <dagmar.prokonsul@gmail.com>
2021-05-03 22:27:21 +0200

    add task - react

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,26 @@
+import { useState } from 'react';
+
 const Main = () => {
+
+  const [taskList, setTaskList] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
+  const addTask = () => {
+    if (taskName) {
+      setTaskList([...taskList, taskName]);
+    }
+    setTaskName('');
+  };
+
+  const taskListContent = taskList.map((task) => {
+    return (
+      <div className='col-md-10 d-flex justify-content-end'>
+        <p className='px-2'>{task}</p>
+        <i class="far fa-trash-alt"></i>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className='row justify-content-center w-100'>
@@ -7,13 +29,17 @@ const Main = () => {
           <div className='d-flex align-items-center justify-content-center w-100'>
             <input
               className='form-control'
+              onChange={(e) => setTaskName(e.target.value)}
               placeholder='enter task'
               type='text'
+              value={taskName}
             />
             <button
               className='btn btn-primary'
+              onClick={addTask}
             >ADD</button>
           </div>
+          {taskListContent}
         </div>
       </div>
     </div>
